e0ea3ea9b728ff41af6ff3a0f4eec1791d70d90d
CristianF09 <cris7i_laurentiu@yahoo.com>
2024-07-02 00:02:11 +0300

    Update Ex 1 + 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,52 @@
 
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+
+const saveFormStateThrottled = throttle(() => {
+
+  const emailValue = form.elements.email.value;
+  const messageValue = form.elements.message.value;
+
+  const formData = {
+    email: emailValue,
+    message: messageValue
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}, 500);
+
+function displaySavedState() {
+
+  const savedState = localStorage.getItem('feedback-form-state');
+  if (savedState) {
+    
+    const formData = JSON.parse(savedState);
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+  }
+}
+
+function handleSubmit(event) {
+
+  event.preventDefault();
+
+  const emailValue = form.elements.email.value;
+  const messageValue = form.elements.message.value;
+
+  const formData = {
+    email: emailValue,
+    message: messageValue
+  };
+  console.log(formData);
+
+  localStorage.removeItem('feedback-form-state');
+
+  form.reset();
+}
+
+form.addEventListener('input', saveFormStateThrottled);
+
+form.addEventListener('submit', handleSubmit);
+
+displaySavedState();
